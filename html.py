c2e391a52f5b4af6d8d5150bc3255d58c14124f1
Liangyue "Yue" Zhao <liangyuezhao7@gmail.com>
2023-08-16 12:36:50 -0400

    Added a method to set inner text.

--- html.py
+++ html.py
@@ -3,30 +3,35 @@ from typing import Dict, List
 
 class Element:
     def __init__(self, name: str):
-        self.name: str = name
-        self.properties: Dict[str, str] = {}
-        self.children: List['Element'] = []
+        self._name: str = name
+        self._properties: Dict[str, str] = {}
+        self._inner_text: str = ""
+        self._children: List['Element'] = []
 
     def property(self, prop_name: str, prop: str) -> 'Element':
-        self.properties[prop_name] = prop
+        self._properties[prop_name] = prop
         return self
 
     def add_child(self, child: 'Element') -> 'Element':
-        self.children.append(child)
+        self._children.append(child)
+        return self
+
+    def inner_text(self, text: str) -> 'Element':
+        self._inner_text = text
         return self
 
     def render(self) -> str:
-        output = f"<{self.name}"
+        output = f"<{self._name}"
 
-        for prop_name, prop in self.properties:
+        for prop_name, prop in self._properties:
             output += f" {prop_name}=\"{prop}\""
 
-        output += ">"
+        output += f">{self.inner_text}"
 
-        if len(self.children) != 0:
-            for child in self.children:
+        if len(self._children) != 0:
+            for child in self._children:
                 output += child.render()
 
-            output += f"</{self.name}>"
+            output += f"</{self._name}>"
 
         return output
