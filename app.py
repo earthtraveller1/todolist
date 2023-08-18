e6f5f1540af1f8a0ce9ffc46aa9fbc5cac4339cf
Liangyue "Yue" Zhao <liangyuezhao7@gmail.com>
2023-08-17 21:19:12 -0400

    Tested out the todo list item component.

--- app.py
+++ app.py
@@ -33,7 +33,11 @@ def index() -> str:
                     Element("div")
                     .id("test-target")
                 )
-                .add_child(components.todolist_container())
+                .add_child(
+                    components.todolist_container()
+                    .add_child(components.todolist_item("Neng Li is the President of China", True))
+                    .add_child(components.todolist_item("Shiva is the King of the Universe!", True))
+                )
             )
         )
 
