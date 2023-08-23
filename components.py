b04370889caa580c28f0eac9f0a71a24a43b6bcc
Liangyue "Yue" Zhao <liangyuezhao7@gmail.com>
2023-08-23 13:45:31 -0400

    Fixed line-too-long warnings from LSP.

--- components.py
+++ components.py
@@ -27,7 +27,8 @@ def head(title: str) -> Element:
             )
             .property(
                 "integrity",
-                "sha384-zUfuhFKKZCbHTY6aRR46gxiqszMk5tcHjsVFxnUo8VMus4kHGVdIYVbOYYNlKmHV"
+                "sha384-zUfuhFKKZCbHTY6aRR46gxiqszMk5tcHjsVFxnUo8V" +
+                "Mus4kHGVdIYVbOYYNlKmHV"
             )
             .property(
                 "crossorigin",
@@ -49,7 +50,8 @@ def todolist_container() -> Element:
 def todolist_item(text: str, is_done: bool) -> Element:
     classes = "rounded-lg text-lime-100 mb-8 p-4 select-none"
     if is_done:
-        classes += " line-through bg-lime-900 hover:bg-lime-700 active:bg-lime-950"
+        classes += " line-through bg-lime-900 "
+        classes += "hover:bg-lime-700 active:bg-lime-950"
     else:
         classes += " bg-indigo-900 hover:bg-indigo-700 active:bg-indigo-950"
 
@@ -62,7 +64,10 @@ def todolist_item(text: str, is_done: bool) -> Element:
 def button(text: str = "Add Item") -> Element:
     return\
         Element("button")\
-        .class_names("mx-auto bg-green-900 rounded-md px-5 py-2 hover:bg-green-800 active:bg-green-950")\
+        .class_names(
+            "mx-auto bg-green-900 rounded-md px-5 py-2 " +
+            "hover:bg-green-800 active:bg-green-950"
+        )\
         .inner_text(text)
 
 
@@ -72,7 +77,11 @@ def create_item_form() -> Element:
         .class_names("flex justify-item-center mx-2")\
         .add_child(
             Element("input")
-            .class_names("bg-slate-900 mr-5 ml-0 border-b-2 focus:border-red-100 duration-300 focus:outline-none border-violet-800")
+            .class_names(
+                "bg-slate-900 mr-5 ml-0 border-b-2 " +
+                "focus:border-red-100 duration-300 " +
+                "focus:outline-none border-violet-800"
+            )
             .property("type", "text")
             .flag("required")
             .property("minlength", "8")
